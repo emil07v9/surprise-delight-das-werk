512a725a6335ad861b0b7be32b2a9f64a244b973
Emilie Schultz <emilieschultz@MacBook-Pro-tilhrende-Emilie.local>
2021-04-19 14:58:17 +0200

    rettelse script

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    document.querySelector("#heks_container").classList.add("animation_heks");
+    document.querySelector("#heks_container").classList.add("hop_op_ned");
     document.querySelector("#heks_container").addEventListener("mousedown", klikHeksHandler);
 }
 
 function klikHeksHandler() {
-    console.log("klikBox1Handler");
-    document.querySelector("#heks_container").classList.add("roter_forsvind");
-    document.querySelector("#heks_sprites").classList.add("roter_forsvind");
+    console.log("klikHeksHandler");
+    document.querySelector("#heks_container").classList.add("pause");
+    document.querySelector("#heks_sprite").classList.add("roter");
+    document.querySelector("#heks_sprite").classList.add("zoom_ud");
+    document.querySelector("#heks_sprite").classList.add("forsvind");
 }
