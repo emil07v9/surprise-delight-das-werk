5ba85819938049c65cff748326b0c9f56fcfcdc7
Emilie Schultz <emilieschultz@MacBook-Pro-tilhrende-Emilie.local>
2021-04-19 15:33:15 +0200

    add zombiehandler og animationer

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    document.querySelector("#heks_container").classList.add("hop");
-    document.querySelector("#heks_container").addEventListener("mousedown", klikHeksHandler);
+    //    document.querySelector("#heks_container").classList.add("hop");
+    document.querySelector("#heks_container").addEventListener("click", klikHeksHandler);
+    document.querySelector("#zombie_container").addEventListener("click", klikZombieHandler);
 }
 
 function klikHeksHandler() {
     console.log("klikHeksHandler");
-    document.querySelector("#heks_container").classList.add("pause");
+    //    document.querySelector("#heks_container").classList.add("pause");
     document.querySelector("#heks_sprite").classList.add("roter");
-    document.querySelector("#heks_sprite").classList.add("zoom_ud");
     document.querySelector("#heks_sprite").classList.add("forsvind");
+}
+
+function klikZombieHandler() {
+    console.log("klikZombieHandler");
+    document.querySelector("#zombie_sprite1").classList.add("zoom_ud");
+    document.querySelector("#zombie_sprite1").classList.add("forsvind");
 }
