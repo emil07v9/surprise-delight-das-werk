5e779969b5ebf4510dbef5cd6890ad160d0926f4
Emilie Schultz <emilieschultz@MacBook-Pro-tilhrende-Emilie.local>
2021-04-20 15:15:51 +0200

    add reset heks og zombie1 samt pos 1-4

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    document.querySelector("#heks_container").classList.add("hop");
-    document.querySelector("#zombie_container1").classList.add("hop");
+    document.querySelector("#heks_container").classList.add("pos1", "hop");
+    document.querySelector("#zombie_container1").classList.add("pos3", "hop");
     document.querySelector("#heks_container").addEventListener("mousedown", klikHeksHandler);
     document.querySelector("#zombie_container1").addEventListener("mousedown", klikZombie1Handler);
 }
@@ -13,6 +13,17 @@ function klikHeksHandler() {
     document.querySelector("#heks_container").removeEventListener("mousedown", klikHeksHandler);
     document.querySelector("#heks_container").classList.add("pause");
     document.querySelector("#heks_sprite").classList.add("roter_forsvind");
+    document.querySelector("#heks_container").addEventListener("animationend", heksReset);
+}
+
+function heksReset() {
+    console.log("heksReset");
+    document.querySelector("#heks_container").classList = "";
+    document.querySelector("#heks_sprite").classList = "";
+    document.querySelector("#heks_container").removeEventListener("animationend", heksReset);
+    document.querySelector("#heks_container").offsetHeight;
+    document.querySelector("#heks_container").classList.add("pos2", "hop");
+    document.querySelector("#heks_container").addEventListener("mousedown", klikHeksHandler);
 }
 
 function klikZombie1Handler() {
@@ -20,4 +31,15 @@ function klikZombie1Handler() {
     document.querySelector("#zombie_container1").removeEventListener("mousedown", klikZombie1Handler);
     document.querySelector("#zombie_container1").classList.add("pause");
     document.querySelector("#zombie_sprite1").classList.add("zoom_forsvind");
+    document.querySelector("#zombie_sprite1").addEventListener("animationend", zombie1Reset);
+}
+
+function zombie1Reset() {
+    console.log("zombie1Reset");
+    document.querySelector("#zombie_container1").classList = "";
+    document.querySelector("#zombie_sprite1").classList = "";
+    document.querySelector("#zombie_container1").removeEventListener("animationend", zombie1Reset);
+    document.querySelector("#zombie_container1").offsetHeight;
+    document.querySelector("#zombie_container1").classList.add("pos4", "hop");
+    document.querySelector("#zombie_container1").addEventListener("mousedown", klikZombie1Handler);
 }
